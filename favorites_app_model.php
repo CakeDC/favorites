cf200f1e5e6ca70d6978568936082faacccbf94c
Alejandro Ibarra <ajibarra@gmail.com>
2011-10-06 15:30:27 +0200

    Added favorites_app_controller and model

---
+++ favorites_app_model.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2009-2010, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2009-2010, Cake Development Corporation (http://cakedc.com)
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+/**
+ * Favorites Plugin AppModel
+ *
+ * @package favorites
+ */
+class FavoritesAppModel extends AppModel {
+
+/**
+ * Plugin name
+ *
+ * @var string $plugin
+ */
+        public $plugin = 'Favorites';
+
+}
